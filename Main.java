81f722f70802040cc0e201d0bc57ed63f98ab395
b18042 <e1b18042@oit.ac.jp>
2022-01-20 16:40:15 +0900

    初期コミット

---
+++ Main.java
@@ -0,0 +1,38 @@
+//課題4
+//import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	//課題4
+        //名前,年齢を入力する機能を追加する
+        //Scanner scanner = new Scanner(System.in);
+    	
+    	//課題2
+    	//名前,年齢をPersonクラスのオブジェクト生成に変更する
+        String name = "KateJones";
+        int age = 20;
+        
+        //課題4
+        //身長,体重を入力する機能
+        
+        //課題3
+    	//身長,体重,BMIをHealthクラスで定義および計算するように変更する
+    	//printDataメソッドを使って名前,年齢,身長,体重,BMIを表示する
+        double height = 1.735;
+        double weight = 67.0;
+        double bmi = weight / height / height;
+        
+		//課題4
+        //scanner.close();
+        
+        //課題1
+        //名前,年齢,身長,体重,BMIの表示
+
+    }
+    //課題3
+	//課題1の名前,年齢,身長,体重,BMIの表示機能をprintDataメソッドに移動
+	//isHealthyメソッドを使ってBMIが標準値を満たしているかの表示機能を追加する
+    //public static void printData(Person person) {
+    //
+	//}
+}
---
+++ Person.java
@@ -0,0 +1,22 @@
+class Person {
+    public String name;
+    public int age;
+    //課題3
+    //メンバ変数にHealthを追加する
+    //public final Health health;
+
+    //課題2
+    //firstName,lastName,ageを引数としたコンストラクタ
+    Person(String firstName, String lastName, int age) {
+        //課題3
+        //healthを初期化
+        //this.health =
+    }
+
+    //課題3
+    //healthを使って身長・体重のデータを更新する
+    //void updateHealth(double height, double weight) {
+    //
+    //}
+
+}
